5b3a63bafaffa9d341295ebc9d315eff81d50e9c
shivpujari <117809630+shivpujari@users.noreply.github.com>
2023-03-02 09:44:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,25 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { todoReducer } from '../reducers/todo';
 import '../styles/App.css';
 import { AddTodo } from './AddTodo';
 import { Todo } from './Todo';
 const App = () => {
+
   const initialValues = [];
-  const [state,dispatch] = useReducer(todoReducer, initialValues)
+  const [state,dispatch] = useReducer(todoReducer,initialValues)
   
-  // useEffect(() => {
-  //   console.log('list - ', state)
-  // })
- 
+  useEffect(() => {
+    console.log('list - ', state)
+  })
  
   return (
     <div id="main">
-      <AddTodo dispatch={dispatch} />
-     {state.map((item, index)=>(
-      <Todo data={item} key={item.id} dispatch={dispatch}/>
-
-     ))
-    }
+      <AddTodo dispatch = {dispatch}/>
+     {
+      state.map((item, index) => (
+        <Todo data={item} key={item.id} dispatch={dispatch}/>
+      ))
+     }
     </div>
   )
 }
