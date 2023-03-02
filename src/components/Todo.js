efe6acc3e42f7096af704ceb00a1e783c9dea017
shivpujari <117809630+shivpujari@users.noreply.github.com>
2023-03-02 09:45:40 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,19 +1,18 @@
 import React from 'react';
 const Todo = (props) => {
-    const {data, dispatch}= props;
+    const {data, dispatch} = props;
     const {id, title} = data;
-
-
+    console.log('props', props);
     return (
-        <div>
-        <div className='todo'>
-        <div className='todo-title'>
-        {title}
-        <input type='checkbox' value={id}/>
-        <button className='todo-delete' onClick={() => dispatch ({type: 'DELETETODO', payload: id})}>Delete</button>
-        </div>
-        </div>
-        </div>
+        <>
+            <div className="todo">
+                <div className='todo-title'>
+                    {title}
+                    <input type="checkbox" value={id}/>
+                    <button className='todo-delete' onClick={() => dispatch({type: 'DELETETODO', payload: id})}>Delete</button>
+                </div>
+            </div>
+        </>
     )
 }
 
