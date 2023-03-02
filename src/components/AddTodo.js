aad30b0cbf75cf319709a9121d5f6d7ef502febc
shivpujari <117809630+shivpujari@users.noreply.github.com>
2023-03-02 09:43:51 +0530

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,25 +1,22 @@
 import React, { useState } from "react"
 
-const AddTodo = ({ dispatch }) => {
+const AddTodo = ({dispatch}) => {
 
     const [tempInput, setTempInput] = useState('');
 
-    const submitFn=(e)=>{
+    const submitFn = (e) => {
         e.preventDefault();
-        dispatch({type: 'ADDTODO', payload:tempInput});
+        dispatch({type: 'ADDTODO', payload: tempInput});
         setTempInput('');
     }
 
-
-
     return (
-        <div>
-        <form id="todo-form" onSubmit={submitFn}>
-        <input type="text" id="todo-input" value={tempInput} onChange={(e)=> setTempInput(e.target.value)}/>
-        <button type="submit">Add To List</button>
-        </form>
-        </div>
-
+        <>
+            <form id="todo-form" onSubmit={submitFn}>
+                <input type="text" onChange={(e) => setTempInput(e.target.value)} id="todo-input" value={tempInput}/>
+                <button type="submit">Add To List</button>
+            </form>
+        </>
     )
 }
 
